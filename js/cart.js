97c904ac3c13f2fac857a22020aaafd05b87b5f8
JuanTejera <jte0001@hotmail.com>
2020-10-24 16:01:32 -0300

    Entrega 6
    Nuevas funcionalidades de del carrito

--- js/cart.js
+++ js/cart.js
@@ -1,37 +1,112 @@
 var msg = "";
+
 var subTotal = 0;
+var totalAmount = 0;
+var shippingCosts = 0;
+
+//Ratios
+var stdRatio = 0.05;
+var expressRatio = 0.07;
+var premiumRatio = 0.15;
+
+
+//Get Inputs
+var stdInput = document.getElementById('std-method');
+var expressInput = document.getElementById('express-method');
+var premiumInput = document.getElementById('premium-method');
+
+
+function calculateTotalAmount(){
+    totalAmount = subTotal + shippingCosts;
+}
+
+function updateTotalAmount(){
+    document.getElementById("totalPrice").innerHTML = `$ `+ totalAmount;
+}
+
+function calculateShippingCosts(shippingRatio) {
+    shippingCosts = shippingRatio * subTotal;
+    return shippingCosts;
+}
+
 
 
 function redirect() {
     window.location.href = "index.html";
 }
 
+function paymentSelected() {
+    sessionStorage.setItem('metodoPago', 'true');
+}
+
+function shippingSelected() {
+    sessionStorage.setItem('shipping', 'true');
+}
+
+function disableCardMethod() {
+    document.getElementById("cvv").disabled = true;
+    document.getElementById("cardNumber").disabled = true;
+    document.getElementById("expDate").disabled = true;
+}
+
+function enableBankMethod() {
+    document.getElementById("accountNumber").disabled = false;
+    disableCardMethod();
+}
+
+function enableCardMethod() {
+    document.getElementById("cvv").disabled = false;
+    document.getElementById("cardNumber").disabled = false;
+    document.getElementById("expDate").disabled = false;
+    document.getElementById("accountNumber").disabled = true;
+
+}
+
 
 //Verifica que se hayan selecionado metodo de pago y de envío y redirecciona si corresponde
 function verifyChecked() {
-    let payMethodSelected = false;
-    let deliveryMethodSelected = false;
-    payMethodSelected = (document.getElementById("credit-card").checked || document.getElementById("transfer").checked);
-    deliveryMethodSelected = (document.getElementById("std-method").checked || document.getElementById("premium-method").checked || document.getElementById("express-method").checked);
-
-    if (payMethodSelected && deliveryMethodSelected) {
+    let datosDeDireccion = sessionStorage.getItem("shipping");
+    let datosPago = sessionStorage.getItem("metodoPago");
+    
+    if ((shippingCosts !== 0) && datosDeDireccion && datosPago) {
         htmlContentToAppend = "";
         htmlContentToAppend = `
         <div style="position:absolute; width: 50%;margin: 0 auto;text-align: center;background-color: LightGreen;" class="ibox-content">`+ msg + `</div>
         `
         document.getElementById("messagePlace").innerHTML = htmlContentToAppend;
+        sessionStorage.removeItem("metodoPago");
+        sessionStorage.removeItem("shipping");
         setTimeout("redirect()", 2000);
-    } else {
-
-        if (!payMethodSelected) {
-            document.getElementById("pagoSelect").style.color = "#ff8080";
-        } else { document.getElementById("pagoSelect").style.color = "#000000"; }
-
-        if (!deliveryMethodSelected) {
+    } else{
+        if (shippingCosts === 0){                //Controla que se haya selecionado metodo de envio
             document.getElementById("envioSelect").style.color = "#ff8080";
-        } else { document.getElementById("envioSelect").style.color = "#000000"; }
-    }
+        } else { document.getElementById("envioSelect").style.color = "#000000";}
 
+        if (datosPago !== true) {
+            document.getElementById("btnDatosPago").className="btn btn-outline-danger";
+        } else {document.getElementById("btnDatosPago").className="btn btn-outline-primary"}
+        if (datosDeDireccion !== true) {
+            document.getElementById("btnDatosEnvio").className="btn btn-outline-danger";
+        } else {document.getElementById("btnDatosPago").className="btn btn-outline-primary"}
+    }
+    
+}
+
+
+function updateShippingCosts() {
+    let stdCost = calculateShippingCosts(stdRatio);
+    let expressCost = calculateShippingCosts(expressRatio);
+    let premiumCost = calculateShippingCosts(premiumRatio);
+    if (stdInput.checked) {
+        shippingCosts = Math.round(stdCost);
+    }
+    if (expressInput.checked) {
+        shippingCosts = Math.round(expressCost);
+    }
+    if (premiumInput.checked) {
+        shippingCosts = Math.round(premiumCost);
+    }
+    document.getElementById("deliveryCost").innerHTML = `$ ` + shippingCosts;
 }
 
 function showItems(array) {
@@ -80,6 +155,9 @@ function showItems(array) {
     document.getElementById("cartArticles").innerHTML = htmlContentToAppend;
 }
 
+
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
@@ -91,13 +169,15 @@ document.addEventListener("DOMContentLoaded", function (e) {
             document.getElementById("subTotalPrice").innerHTML = `$` + subTotal;
         }
 
-        function changeSubTotal(){
+        function changeSubTotal() {
             newSubTotal = 0;
             for (let i = 0; i < articulos.length; i++) {
                 newSubTotal += parseInt(document.getElementById("valueProd" + i).value) * articulos[i].unitCost;
             }
             document.getElementById("subTotalPrice").innerHTML = `$` + newSubTotal;
+            subTotal = newSubTotal;
         }
+
 
         for (let i = 0; i < articulos.length; i++) {
 
@@ -109,7 +189,10 @@ document.addEventListener("DOMContentLoaded", function (e) {
                     document.getElementById("valueProd" + i).value--;
                     tot = parseInt(document.getElementById("valueProd" + i).value) * articulos[i].unitCost;
                     document.getElementById("sumPrice" + i).innerHTML = "$" + tot;
-                    changeSubTotal()
+                    changeSubTotal();
+                    if (shippingCosts !== 0) {updateShippingCosts();}
+                    calculateTotalAmount();
+                    updateTotalAmount();
                 }
             })
 
@@ -118,17 +201,40 @@ document.addEventListener("DOMContentLoaded", function (e) {
                 tot = parseInt(document.getElementById("valueProd" + i).value) * articulos[i].unitCost;
                 document.getElementById("sumPrice" + i).innerHTML = "$" + tot;
                 changeSubTotal();
+                if (shippingCosts !== 0) {updateShippingCosts();}
+                calculateTotalAmount();
+                updateTotalAmount();
 
             })
 
             document.getElementById("valueProd" + i).addEventListener("keyup", function () {            //Implementa poder digitar la cant de articulos en tiempo real 
-                if (parseInt(document.getElementById("valueProd" + i).value) < 1){ document.getElementById("valueProd" + i).value = 1 }
+                if (parseInt(document.getElementById("valueProd" + i).value) < 1) { document.getElementById("valueProd" + i).value = 1 }
                 tot = parseInt(document.getElementById("valueProd" + i).value) * articulos[i].unitCost;
                 document.getElementById("sumPrice" + i).innerHTML = "$" + tot;
                 changeSubTotal();
             })
         }
 
+        stdInput.addEventListener("change", function (event) {
+                shippingCosts = Math.round(calculateShippingCosts(stdRatio));                                    //Actualiza valores al selecionar distos metodos de envio
+                document.getElementById("deliveryCost").innerHTML = `$ ` + shippingCosts;
+                calculateTotalAmount();
+                updateTotalAmount();
+        });
+
+        expressInput.addEventListener("change", function (event) {
+                shippingCosts = Math.round(calculateShippingCosts(expressRatio));
+                document.getElementById("deliveryCost").innerHTML = `$ ` + shippingCosts;
+                calculateTotalAmount();
+                updateTotalAmount();
+        });
+
+        premiumInput.addEventListener("change", function (event) {
+                shippingCosts = Math.round(calculateShippingCosts(premiumRatio));
+                document.getElementById("deliveryCost").innerHTML = `$ ` + shippingCosts;
+                calculateTotalAmount();
+                updateTotalAmount();
+        });
 
         document.querySelector("body > nav ").remove();
         getJSONData(CART_BUY_URL).then(function (resultObj) {        //Borra el navBar
